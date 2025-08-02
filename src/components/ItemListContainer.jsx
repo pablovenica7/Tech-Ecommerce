8dba5e239207b6ffa56de5b8b829228f99da342b
Pablo Venica <pablo.venica7@gmail.com>
2025-08-02 12:09:15 -0300

    Update ItemListContainer.jsx

--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -1,9 +1,7 @@
-const ItemListContainer = ({ greeting }) => {
+export function ItemListContainer(props) {
   return (
     <section className="container mt-5">
-      <h2>{greeting}</h2>
+      <h2>{props.mensaje}</h2>
     </section>
   );
-};
-
-export default ItemListContainer;
+}
