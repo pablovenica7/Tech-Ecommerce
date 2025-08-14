0cd174e5933256caf85e88d4e20d10cc521576e2
Pablo Venica <pablo.venica7@gmail.com>
2025-08-14 12:55:25 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import { NavBar } from './components/NavBar';
 import { ItemListContainer } from './components/ItemListContainer';
 import { Footer } from './components/Footer';
+import { ListaProductos } from './components/ListaProductos';
+import { Contador } from './components/Contador';
 
 function App() {
   return (
     <>
       <NavBar />
       <ItemListContainer mensaje="¡Bienvenido a VEYOR!" />
+      <ListaProductos />
+      <Contador />
       <Footer />
     </>
   );
