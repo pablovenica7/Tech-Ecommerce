71397bc6a49f925abbff9e7e3564aa602815e8b2
Pablo Venica <pablo.venica7@gmail.com>
2025-08-18 15:35:00 -0300

    Update NavBar.jsx

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,18 +1,19 @@
 import { CartWidget } from './CartWidget';
+import { Link } from 'react-router-dom';
 
 export function NavBar() {
   return (
-    <nav className="navbar navbar-expand-lg bg-body-tertiary rounded" aria-label="Thirteenth navbar example">
+    <nav className="navbar navbar-expand-lg bg-body-tertiary rounded">
       <div className="container-fluid">
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarsExample11" aria-controls="navbarsExample11" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
         <div className="collapse navbar-collapse d-lg-flex" id="navbarsExample11">
-          <a className="navbar-brand col-lg-3 me-0" href="#">VEYOR</a>
+          <Link className="navbar-brand col-lg-3 me-0" to="/">VEYOR</Link>
           <ul className="navbar-nav col-lg-6 justify-content-lg-center">
-            <li className="nav-item"><a className="nav-link active" href="#">Inicio</a></li>
-            <li className="nav-item"><a className="nav-link" href="#">PCs de Escritorio</a></li>
-            <li className="nav-item"><a className="nav-link" href="#">Contacto</a></li>
+            <li className="nav-item"><Link className="nav-link" to="/">Inicio</Link></li>
+            <li className="nav-item"><Link className="nav-link" to="/pcs">PCs de Escritorio</Link></li>
+            <li className="nav-item"><Link className="nav-link" to="/contacto">Contacto</Link></li>
           </ul>
           <div className="d-lg-flex col-lg-3 justify-content-lg-end">
             <CartWidget />
@@ -22,5 +23,3 @@ export function NavBar() {
     </nav>
   );
 }
-
-
