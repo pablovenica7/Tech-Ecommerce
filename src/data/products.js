db91774e90384475c3d14dcb5e352af579c0db3a
Pablo Venica <pablo.venica7@gmail.com>
2025-08-28 19:06:44 -0300

    Update products.js

--- src/data/products.js
+++ src/data/products.js
@@ -1,51 +1,56 @@
+// src/data/products.js
+import pcGamer from "../assets/pc-gamer.png";
+import notebook from "../assets/notebook.png";
+import monitor from "../assets/monitor.png";
+
 const PRODUCTS = [
   { 
-    id: 1, 
-    nombre: "PC Gamer Ryzen 5", 
-    precio: 580000, 
-    categoria: "pcs", 
-    descripcion: "PC gamer equilibrada para 1080p.", 
-    img: "/assets/imagenes/pc-gamer.png" 
+    id: 1,
+    nombre: "PC Gamer Ryzen 5",
+    precio: 580000,
+    categoria: "pcs",
+    descripcion: "PC gamer equilibrada para 1080p.",
+    img: pcGamer
   },
   { 
-    id: 2, 
-    nombre: "Notebook Dell i7", 
-    precio: 720000, 
-    categoria: "notebooks", 
-    descripcion: "Notebook potente para trabajo y estudio.", 
-    img: "/assets/imagenes/notebook.png" 
+    id: 2,
+    nombre: "Notebook Dell i7",
+    precio: 720000,
+    categoria: "notebooks",
+    descripcion: "Notebook potente para trabajo y estudio.",
+    img: notebook
   },
   { 
-    id: 3, 
-    nombre: "Monitor 27'' LG", 
-    precio: 250000, 
-    categoria: "monitores", 
-    descripcion: "Panel IPS Full HD con 75Hz.", 
-    img: "/assets/imagenes/monitor.png" 
+    id: 3,
+    nombre: "Monitor 27'' LG",
+    precio: 250000,
+    categoria: "monitores",
+    descripcion: "Panel IPS Full HD con 75Hz.",
+    img: monitor
   },
   { 
-    id: 4, 
-    nombre: "PC Office i5", 
-    precio: 430000, 
-    categoria: "pcs", 
-    descripcion: "Ideal para ofimática y multimedia.", 
-    img: "/assets/imagenes/pc-gamer.png" 
+    id: 4,
+    nombre: "PC Office i5",
+    precio: 430000,
+    categoria: "pcs",
+    descripcion: "Ideal para ofimática y multimedia.",
+    img: pcGamer
   },
   { 
-    id: 5, 
-    nombre: "Notebook Lenovo Ryzen 7", 
-    precio: 810000, 
-    categoria: "notebooks", 
-    descripcion: "Rendimiento portátil de alta gama.", 
-    img: "/assets/imagenes/notebook.png" 
+    id: 5,
+    nombre: "Notebook Lenovo Ryzen 7",
+    precio: 810000,
+    categoria: "notebooks",
+    descripcion: "Rendimiento portátil de alta gama.",
+    img: notebook
   },
   { 
-    id: 6, 
-    nombre: "Monitor 24'' Samsung", 
-    precio: 190000, 
-    categoria: "monitores", 
-    descripcion: "Compacto y nítido para oficina.", 
-    img: "/assets/imagenes/monitor.png" 
+    id: 6,
+    nombre: "Monitor 24'' Samsung",
+    precio: 190000,
+    categoria: "monitores",
+    descripcion: "Compacto y nítido para oficina.",
+    img: monitor
   },
 ];
 
