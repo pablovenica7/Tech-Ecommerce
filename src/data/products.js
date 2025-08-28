232e5e6b32d920cf402dd29871e50cf3971977da
Pablo Venica <pablo.venica7@gmail.com>
2025-08-28 18:48:59 -0300

    Update products.js

--- src/data/products.js
+++ src/data/products.js
@@ -1,10 +1,52 @@
 const PRODUCTS = [
-  { id: 1, nombre: "PC Gamer Ryzen 5",        precio: 580000, categoria: "pcs",       descripcion: "PC gamer equilibrada para 1080p." },
-  { id: 2, nombre: "Notebook Dell i7",        precio: 720000, categoria: "notebooks", descripcion: "Notebook para trabajo y estudio." },
-  { id: 3, nombre: "Monitor 27'' LG",         precio: 250000, categoria: "monitores", descripcion: "Panel IPS Full HD 75Hz." },
-  { id: 4, nombre: "PC Office i5",            precio: 430000, categoria: "pcs",       descripcion: "Para ofimática y multimedia." },
-  { id: 5, nombre: "Notebook Lenovo Ryzen 7", precio: 810000, categoria: "notebooks", descripcion: "Rendimiento portátil alto." },
-  { id: 6, nombre: "Monitor 24'' Samsung",    precio: 190000, categoria: "monitores", descripcion: "Compacto y nítido para oficina." },
+  { 
+    id: 1, 
+    nombre: "PC Gamer Ryzen 5", 
+    precio: 580000, 
+    categoria: "pcs", 
+    descripcion: "PC gamer equilibrada para 1080p.", 
+    img: "/assets/imagenes/pc-gamer.png" 
+  },
+  { 
+    id: 2, 
+    nombre: "Notebook Dell i7", 
+    precio: 720000, 
+    categoria: "notebooks", 
+    descripcion: "Notebook potente para trabajo y estudio.", 
+    img: "/assets/imagenes/notebook.png" 
+  },
+  { 
+    id: 3, 
+    nombre: "Monitor 27'' LG", 
+    precio: 250000, 
+    categoria: "monitores", 
+    descripcion: "Panel IPS Full HD con 75Hz.", 
+    img: "/assets/imagenes/monitor.png" 
+  },
+  { 
+    id: 4, 
+    nombre: "PC Office i5", 
+    precio: 430000, 
+    categoria: "pcs", 
+    descripcion: "Ideal para ofimática y multimedia.", 
+    img: "/assets/imagenes/pc-gamer.png" 
+  },
+  { 
+    id: 5, 
+    nombre: "Notebook Lenovo Ryzen 7", 
+    precio: 810000, 
+    categoria: "notebooks", 
+    descripcion: "Rendimiento portátil de alta gama.", 
+    img: "/assets/imagenes/notebook.png" 
+  },
+  { 
+    id: 6, 
+    nombre: "Monitor 24'' Samsung", 
+    precio: 190000, 
+    categoria: "monitores", 
+    descripcion: "Compacto y nítido para oficina.", 
+    img: "/assets/imagenes/monitor.png" 
+  },
 ];
 
 export function fetchProducts(delayMs = 1000) {
@@ -19,4 +61,3 @@ export function fetchProductById(id, delayMs = 800) {
     }, delayMs);
   });
 }
-
