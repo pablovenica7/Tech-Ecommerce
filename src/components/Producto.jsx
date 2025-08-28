05bb22eb7ec0d098da2ec27881b222aace46447d
Pablo Venica <pablo.venica7@gmail.com>
2025-08-28 18:47:04 -0300

    Update Producto.jsx

--- src/components/Producto.jsx
+++ src/components/Producto.jsx
@@ -1,11 +1,17 @@
 import { Link } from "react-router-dom";
 
-export function Producto({ id, nombre, precio }) {
+export function Producto({ id, nombre, precio, img }) {
   return (
     <div className="card h-100 shadow-sm">
+      {img && (
+        <img src={img} alt={nombre} className="card-img-top" />
+      )}
+
       <div className="card-body d-flex flex-column">
         <h5 className="card-title">{nombre}</h5>
-        <p className="card-text text-muted mb-4">Precio: ${precio.toLocaleString()}</p>
+        <p className="card-text text-muted mb-4">
+          Precio: ${precio.toLocaleString()}
+        </p>
 
         <div className="mt-auto d-flex gap-2">
           <Link className="btn btn-outline-dark" to={`/producto/${id}`}>
