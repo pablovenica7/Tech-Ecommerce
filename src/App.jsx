f8d0d7bdd7eef6f1f2c4153873c516b98067185d
Pablo Venica <pablo.venica7@gmail.com>
2025-09-15 15:11:43 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,6 @@ import { NavBar } from "./components/NavBar";
 import { ItemListContainer } from "./components/ItemListContainer";
 import { ItemDetailContainer } from "./components/ItemDetailContainer";
 import { Cart } from "./components/Cart";
-import { Checkout } from "./components/Checkout";
 import { Footer } from "./components/Footer";
 
 function App() {
@@ -17,7 +16,6 @@ function App() {
           <Route path="/categoria/:id" element={<ItemListContainer />} />
           <Route path="/detalle/:id" element={<ItemDetailContainer />} />
           <Route path="/carrito" element={<Cart />} />
-          <Route path="/checkout" element={<Checkout />} />
         </Routes>
         <Footer />
       </BrowserRouter>
