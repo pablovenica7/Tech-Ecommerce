3741c07f379178654fd9930c2e878c6e7e6969a9
Pablo Venica <pablo.venica7@gmail.com>
2025-09-15 15:44:01 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { CartProvider } from "./context/CartContext";
+
 import NavBar from "./components/NavBar";
 import Footer from "./components/Footer";
-import Home from "./pages/Home";
-import Contacto from "./pages/Contacto";
+
 import ItemListContainer from "./components/ItemListContainer";
 import ItemDetailContainer from "./components/ItemDetailContainer";
+
+// Páginas
+import Home from "./pages/Home";        // opcional, lo dejamos en /home
+import Contacto from "./pages/Contacto";
 import Cart from "./pages/Cart";
 import Checkout from "./pages/Checkout";
 
@@ -14,15 +18,31 @@ function App() {
     <CartProvider>
       <BrowserRouter>
         <NavBar />
+
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/contacto" element={<Contacto />} />
+          {/* Consigna: / => Catálogo */}
+          <Route path="/" element={<ItemListContainer />} />
+
+          {/* Catálogo explícito */}
           <Route path="/catalogo" element={<ItemListContainer />} />
+
+          {/* Categorías y detalle */}
           <Route path="/categoria/:idCategoria" element={<ItemListContainer />} />
-          <Route path="/producto/:idProducto" element={<ItemDetailContainer />} />
+          <Route path="/detalle/:idProducto" element={<ItemDetailContainer />} />
+
+          {/* Carrito y Checkout */}
           <Route path="/carrito" element={<Cart />} />
           <Route path="/checkout" element={<Checkout />} />
+
+          {/* Extras (opcional) */}
+          <Route path="/home" element={<Home />} />
+          <Route path="/contacto" element={<Contacto />} />
+
+          {/* Redirecciones útiles */}
+          <Route path="/producto/:idProducto" element={<Navigate to="/detalle/:idProducto" replace />} />
+          <Route path="*" element={<Navigate to="/" replace />} />
         </Routes>
+
         <Footer />
       </BrowserRouter>
     </CartProvider>
