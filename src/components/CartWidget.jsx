0a50550e33bca271218a212f507fb1be01de6ec7
Pablo Venica <pablo.venica7@gmail.com>
2025-09-15 15:41:22 -0300

    Update CartWidget.jsx

--- src/components/CartWidget.jsx
+++ src/components/CartWidget.jsx
@@ -1,12 +1,13 @@
 import { Link } from "react-router-dom";
 import { useCart } from "../context/CartContext";
-import carritoIcon from "../assets/icono_carrito.png";
+import iconCart from "../assets/icono_carrito.png";
 
 export default function CartWidget() {
   const { totalItems } = useCart();
+
   return (
-    <Link to="/carrito" className="cart-widget">
-      <img src={carritoIcon} alt="Carrito" className="cart-icon" />
+    <Link to="/carrito" className="cart-widget" aria-label="Ir al carrito">
+      <img src={iconCart} alt="Carrito" className="cart-icon" />
       {totalItems() > 0 && <span className="cart-count">{totalItems()}</span>}
     </Link>
   );
