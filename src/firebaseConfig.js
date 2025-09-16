0bc80b5ed76768fc7826ec676624df3ea79428c7
Pablo Venica <pablo.venica7@gmail.com>
2025-09-16 08:57:49 -0300

    Add files via upload

---
+++ src/firebaseConfig.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "TU_API_KEY",
+  authDomain: "TU_AUTH_DOMAIN",
+  projectId: "TU_PROJECT_ID",
+  storageBucket: "TU_STORAGE_BUCKET",
+  messagingSenderId: "TU_SENDER_ID",
+  appId: "TU_APP_ID"
+};
+
+export const app = initializeApp(firebaseConfig);
