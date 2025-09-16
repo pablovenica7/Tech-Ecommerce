78177f0d11f9e0142fffe1f67d7defea4ef5a9ae
Pablo Venica <pablo.venica7@gmail.com>
2025-09-16 09:01:33 -0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,7 @@ import App from "./App.jsx";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import { CartProvider } from "./components/CartContext.jsx";
+import "./firebaseConfig";
 
 const DOMElement = document.getElementById("root");
 const VDOMElement = createRoot(DOMElement);
