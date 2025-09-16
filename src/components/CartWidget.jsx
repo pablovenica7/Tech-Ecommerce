392a2707173be0f82fc223e94f9c17df5dcc38f3
Pablo Venica <pablo.venica7@gmail.com>
2025-09-16 08:51:59 -0300

    Update CartWidget.jsx

--- src/components/CartWidget.jsx
+++ src/components/CartWidget.jsx
@@ -1,10 +1,23 @@
 import carrito from "../assets/icono_carrito.png";
 import user from "../assets/icono_user.png";
+import { useCart } from "./CartContext";
 
 export function CartWidget() {
+  const { totalUnidades } = useCart();
+
   return (
     <div className="d-flex align-items-center gap-3">
-      <img src={carrito} alt="Carrito" className="icono-navbar" />
+      <div className="position-relative">
+        <img src={carrito} alt="Carrito" className="icono-navbar" />
+        {totalUnidades > 0 && (
+          <span
+            className="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger"
+            style={{ fontSize: "0.75rem" }}
+          >
+            {totalUnidades}
+          </span>
+        )}
+      </div>
       <img src={user} alt="Usuario" className="icono-navbar" />
     </div>
   );
