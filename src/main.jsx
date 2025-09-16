681f8d9e861dd666c6dba8477ab5dfa808eccc9a
Pablo Venica <pablo.venica7@gmail.com>
2025-09-16 08:51:45 -0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,18 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
+import { CartProvider } from "./components/CartContext.jsx";
 
-const DOMElement = document.getElementById('root')
-const VDOMElement = createRoot(DOMElement)
+const DOMElement = document.getElementById("root");
+const VDOMElement = createRoot(DOMElement);
 
 VDOMElement.render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <CartProvider>
+      <App />
+    </CartProvider>
+  </StrictMode>
+);
