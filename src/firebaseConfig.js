89558d95ac87e18e80a996d2f9fcb319a9d1da7c
Pablo Venica <pablo.venica7@gmail.com>
2025-09-22 15:46:46 -0300

    Update firebaseConfig.js

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,4 +1,5 @@
 import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyAd-f-V4dIEdk0M2JTx9AzWsHY9CMq6ekA",
@@ -11,3 +12,4 @@ const firebaseConfig = {
 };
 
 export const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
